4cc4455c9e0003020278f4685c128dbfd59b9e6d
Andrii Kohut <kogut.andriy@gmail.com>
2022-07-26 20:00:46 +0300

    delete with stream state test

--- tests/streams/test_delete.py
+++ tests/streams/test_delete.py
@@ -3,7 +3,7 @@ import uuid
 import grpc
 import pytest
 
-from streams import DeleteResult
+from streams import DeleteResult, StreamState
 
 
 def test_delete_stream(client):
@@ -37,3 +37,29 @@ def test_delete_stream_with_revision(client):
 
     assert "Expected version: 1, Actual version: 2" in str(err.value)
     client.streams.delete(stream=stream, revision=2)
+
+
+def test_delete_with_stream_state(client):
+    stream1 = str(uuid.uuid4())
+    stream2 = str(uuid.uuid4())
+    stream3 = str(uuid.uuid4())
+    client.streams.append(stream=stream1, event_type="foobar", data=b"")
+    client.streams.append(stream=stream2, event_type="foobar", data=b"")
+
+    client.streams.delete(stream=stream1, stream_state=StreamState.STREAM_EXISTS)
+    # TODO: this should be a custom exception
+    with pytest.raises(grpc._channel._InactiveRpcError) as err:
+        client.streams.delete(stream=stream2, stream_state=StreamState.NO_STREAM)
+
+    assert "Expected version: -1, Actual version: 0" in str(err)
+
+    with pytest.raises(grpc._channel._InactiveRpcError):
+        # This doesn't make sense, since why would someone would delete stream if they think it doesn't exist?
+        client.streams.delete(stream=stream3, stream_state=StreamState.NO_STREAM)
+
+    with pytest.raises(grpc._channel._InactiveRpcError) as err:
+        client.streams.delete(stream=stream3)
+
+    assert "Expected version: -2, Actual version: -1" in str(err)
+
+
