0a97d8c675b5c5db32673cccc576f1ef209bd46f
Andrii Kohut <kogut.andriy@gmail.com>
2022-07-26 18:58:23 +0300

    some more tests for backwards and count

--- tests/streams/test_read.py
+++ tests/streams/test_read.py
@@ -26,3 +26,37 @@ def test_append_and_read(data, client, expected_content_type):
     assert response.metadata == {"created": ANY, "content-type": expected_content_type, "type": "foobar"}
     assert response.stream_name == stream
     assert response.custom_metadata == {"raisedBy": "me"}
+
+
+def test_read_count(client):
+    stream = str(uuid.uuid4())
+    for i in range(20):
+        client.streams.append(stream=stream, event_type="foobar", data={"i": i})
+
+    expected_events = list(range(20))
+    reversed_events = list(reversed(expected_events))
+
+    all_events = [e for e in client.streams.read(stream=stream, count=20)]
+    all_events_backwards = [e for e in client.streams.read(stream=stream, count=20, backwards=True)]
+    first_ten = [e for e in client.streams.read(stream=stream, count=10)]
+    first_ten_backwards = [e for e in client.streams.read(stream=stream, count=10, backwards=True)]
+    last_ten = [e for e in client.streams.read(stream=stream, count=10, revision=10)]
+    last_ten_backwards = [e for e in client.streams.read(stream=stream, count=10, revision=9, backwards=True)]
+
+    assert len(all_events) == 20
+    assert [e.data["i"] for e in all_events] == expected_events
+
+    assert len(all_events_backwards) == 20
+    assert [e.data["i"] for e in all_events_backwards] == reversed_events
+
+    assert len(first_ten) == 10
+    assert [e.data["i"] for e in first_ten] == expected_events[:10]
+
+    assert len(first_ten_backwards) == 10
+    assert [e.data["i"] for e in first_ten_backwards] == reversed_events[:10]
+
+    assert len(last_ten) == 10
+    assert [e.data["i"] for e in last_ten] == expected_events[10:]
+
+    assert len(last_ten_backwards) == 10
+    assert [e.data["i"] for e in last_ten_backwards] == reversed_events[10:]
