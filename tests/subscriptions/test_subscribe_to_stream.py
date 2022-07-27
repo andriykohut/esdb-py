f1fd9e6c357cb9e1d6a0481b76e8c56214537801
Andrii Kohut <kogut.andriy@gmail.com>
2022-07-27 23:59:47 +0300

    initial implementation of persistent subscriptions + basic test

--- tests/subscriptions/test_subscribe_to_stream.py
+++ tests/subscriptions/test_subscribe_to_stream.py
@@ -6,25 +6,29 @@ import grpc
 import pytest
 
 
-def test_create_subscription(client):
+def test_subscribe_to_stream(client):
     stream = f"stream-{str(uuid.uuid4())}"
     group = f"group-{str(uuid.uuid4())}"
 
     responses = []
 
+    # emit some events to the same stream
     for _ in range(10):
         with threading.Lock():
             client.streams.append(stream, "foobar", b"data")
 
+    # create a subscription
     client.subscriptions.create_stream_subscription(stream=stream, group_name=group, revision=1)
 
     deadline = time.time() + 3
 
+    # wait for 10 responses or stop after 3 seconds
     def handler(response, reader):
         responses.append(response)
         if len(responses) == 10:
             reader.cancel()
         if time.time() >= deadline:
+            reader.cancel()
             pytest.fail("Deadline exceeded")
 
     with pytest.raises(grpc._channel._MultiThreadedRendezvous) as err:
