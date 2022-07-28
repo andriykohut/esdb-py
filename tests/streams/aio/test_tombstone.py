db7a4b5ab616af08babfcb65e8b3e1529cc104d7
Andrii Kohut <kogut.andriy@gmail.com>
2022-07-28 12:51:15 +0300

    async tombstone test

--- tests/streams/aio/test_tombstone.py
+++ tests/streams/aio/test_tombstone.py
@@ -6,21 +6,23 @@ import pytest
 from client.streams.base import TombstoneResult
 
 
-def test_tombstone(client):
+@pytest.mark.asyncio
+async def test_tombstone(async_client):
     stream = str(uuid.uuid4())
-    client.streams.append(stream=stream, event_type="foo", data=b"")
-    result = client.streams.tombstone(stream=stream)
+    await async_client.streams.append(stream=stream, event_type="foo", data=b"")
+    result = await async_client.streams.tombstone(stream=stream)
     assert isinstance(result, TombstoneResult)
     assert result.commit_position > 0
     assert result.prepare_position > 0
     # TODO: This should be a custom exception
-    with pytest.raises(grpc._channel._MultiThreadedRendezvous) as err:
-        next(client.streams.read(stream=stream, count=20))
+    with pytest.raises(grpc.aio._call.AioRpcError) as err:
+        async for _ in async_client.streams.read(stream=stream, count=20):
+            ...
 
     assert f"Event stream '{stream}' is deleted." in str(err.value)
 
     # TODO: This should be a custom exception
-    with pytest.raises(grpc._channel._InactiveRpcError) as err:
-        client.streams.append(stream=stream, event_type="foo", data=b"")
+    with pytest.raises(grpc.aio._call.AioRpcError) as err:
+        await async_client.streams.append(stream=stream, event_type="foo", data=b"")
 
     assert f"Event stream '{stream}' is deleted." in str(err.value)
